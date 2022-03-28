f997e32476d05586b2d0392b5cbee4bd0e257b6a
João Pedro Parisi <joaopparisi@gmail.com>
2022-03-28 10:37:14 -0300

    Adicionando as tabelas

--- Escola.sql
+++ Escola.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE dbEscola
 USE dbEscola
 
+CREATE TABLE tbAluno(
+    Ra_Aluno INT PRIMARY KEY,
+    Nome_Aluno VARCHAR(50),
+    Telefone_Aluno VARCHAR(50)
+)
+
+CREATE TABLE tbProfessor(
+    Ra_Professor INT PRIMARY KEY,
+    Nome_Professor VARCHAR(50),
+    Telefone_Professor VARCHAR(50)
+)
